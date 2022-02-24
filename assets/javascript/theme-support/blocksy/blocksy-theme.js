a36cfadd81eb453243ca0a685c4b32b6c8219ef5
nevin-riot <nevin-riot@outlook.es>
2022-02-24 18:43:20 +0100

    Added todo and removed innecessary comments

--- assets/javascript/theme-support/blocksy/blocksy-theme.js
+++ assets/javascript/theme-support/blocksy/blocksy-theme.js
@@ -1,3 +1,4 @@
+// TODO: Make different files for TS files and JS binaries
 jQuery((function ($) {
     var vidiwoo = "vidiwoo";
     if (jQuery(vidiwoo)) {
@@ -21,22 +22,18 @@ jQuery((function ($) {
         window.setTimeout(function () {
             var video;
             blocksyVideos_1.videosProducto.each(function anadirVideo() {
-                // Anadir un Thumbail
+                // TODO: Make thumbnail customized image for video
+                // Anadir un Thumbnail
                 blocksyVideos_1.miniaturaVideo = blocksyImagenes_1.miniaturaImagen.last().clone(true);
                 blocksyVideos_1.miniaturaVideo.children("span").attr("aria-label", "Diapositiva ".concat(blocksyImagenes_1.miniaturaImagen.length + 1));
                 blocksyVideos_1.miniaturaVideo.appendTo(blocksyImagenes_1.galeriaMiniaturas);
+                // TODO: Make videos dimension responsive
                 // Anadir video a la cola de imagenes
                 blocksyVideos_1.cajaVideos = blocksyImagenes_1.cajaImagenes.children("div").last().clone(true);
                 video = jQuery(this).detach();
                 blocksyVideos_1.cajaVideos.html(video.html());
                 blocksyVideos_1.cajaVideos.find("video").attr("width", "600px").attr("height", "600px").css("margin-top", "-100px").css("background-color", "white");
                 blocksyVideos_1.cajaVideos.appendTo(blocksyImagenes_1.cajaImagenes);
-                // // Anadir video a la cola de imagenes
-                // blocksyVideos.cajaVideos = blocksyImagenes.cajaImagenes.children("div").last().clone(true);
-                // blocksyVideos.cajaVideos.html(jQuery(this).html());
-                // blocksyVideos.cajaVideos.children("video").attr("width", "600px").attr("height", "600px").css("margin-top", "-100px").css("background-color", "white");
-                // jQuery(this).hide();
-                // blocksyVideos.cajaVideos.appendTo(blocksyImagenes.cajaImagenes);
             });
         }, e);
     }
--- assets/javascript/theme-support/blocksy/blocksy-theme.ts
+++ assets/javascript/theme-support/blocksy/blocksy-theme.ts
@@ -1,61 +1,60 @@
+    // TODO: Make different files for TS files and JS binaries
+
 jQuery((function ($) {
     let vidiwoo = "vidiwoo"
-    if(jQuery(vidiwoo)){
+    if (jQuery(vidiwoo)) {
         class Imagenes {
             cajaImagenes: JQuery;
             galeriaMiniaturas: JQuery;
             miniaturaImagen: JQuery;
-    
+
             constructor(claseCaja: string, claseMiniaturas: string) {
                 this.cajaImagenes = jQuery(`div.${claseCaja}`);
                 this.galeriaMiniaturas = jQuery(`.${claseMiniaturas} > ol`);
                 this.miniaturaImagen = jQuery(`.${claseMiniaturas} ol > li`);
             }
         }
-    
+
         class Videos {
             cajaVideos: JQuery;
             videosProducto: JQuery;
             miniaturaVideo: JQuery;
-    
+
             constructor(claseVideo: string) {
                 this.videosProducto = jQuery(`.${claseVideo}`);
             }
         }
-    
-    
+
+
         const blocksyImagenes = new Imagenes("flexy-items", "flexy-pills");
         const blocksyVideos = new Videos(vidiwoo);
-    
-    
+
+
         type retrasoEjecucion = number;
         let e: retrasoEjecucion = 250;
-    
-    
-    
+
+
+
         window.setTimeout(function () {
             let video;
             blocksyVideos.videosProducto.each(
                 function anadirVideo() {
-    
-                    // Anadir un Thumbail
+
+
+                    // TODO: Make thumbnail customized image for video
+
+                    // Anadir un Thumbnail
                     blocksyVideos.miniaturaVideo = blocksyImagenes.miniaturaImagen.last().clone(true);
                     blocksyVideos.miniaturaVideo.children("span").attr("aria-label", `Diapositiva ${blocksyImagenes.miniaturaImagen.length + 1}`);
                     blocksyVideos.miniaturaVideo.appendTo(blocksyImagenes.galeriaMiniaturas);
-    
+
+                    // TODO: Make videos dimension responsive
                     // Anadir video a la cola de imagenes
                     blocksyVideos.cajaVideos = blocksyImagenes.cajaImagenes.children("div").last().clone(true)
                     video = jQuery(this).detach()
                     blocksyVideos.cajaVideos.html(video.html())
                     blocksyVideos.cajaVideos.find("video").attr("width", "600px").attr("height", "600px").css("margin-top", "-100px").css("background-color", "white");
                     blocksyVideos.cajaVideos.appendTo(blocksyImagenes.cajaImagenes)
-                    
-                    // // Anadir video a la cola de imagenes
-                    // blocksyVideos.cajaVideos = blocksyImagenes.cajaImagenes.children("div").last().clone(true);
-                    // blocksyVideos.cajaVideos.html(jQuery(this).html());
-                    // blocksyVideos.cajaVideos.children("video").attr("width", "600px").attr("height", "600px").css("margin-top", "-100px").css("background-color", "white");
-                    // jQuery(this).hide();
-                    // blocksyVideos.cajaVideos.appendTo(blocksyImagenes.cajaImagenes);
                 })
         }, e)
     }
